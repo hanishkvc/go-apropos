b8325f26e2a7a6fc99be680f957a944adc5c895c
hanishkvc <hanishkvc@gmail.com>
2022-06-02 22:44:21 +0530

    Main: Some test code wrt understanding go

--- main.go
+++ main.go
@@ -14,7 +14,33 @@ const PRG_VERSION = "v0-20220602IST0954"
 var gFind string
 var gBasePath string = "/usr/lib/go-1.18/"
 
+func test_go() {
+	var anIntSlice []int = []int{1, 2, 3}
+	var assignedSlice []int
+	anIntSlice = append(anIntSlice, 4, 5, 6)
+	anIntSlice = append(anIntSlice, []int{10, 20, 30}...)
+	fmt.Printf("anIntArray: %v\n", anIntSlice)
+	fmt.Printf("len(anIntSlice): %v\n", len(anIntSlice))
+	var anotherSlice []int
+	fmt.Printf("anotherSlice: %v\n", anotherSlice)
+	fmt.Printf("len(anotherSlice): %v\n", len(anotherSlice))
+	assignedSlice = anIntSlice
+	assignedSlice[1] = 999
+	fmt.Printf("assignedSlice: %v\n", assignedSlice)
+	fmt.Printf("anIntSlice: %v\n", anIntSlice)
+	// arrays
+	var aArray [3]int = [3]int{10, 20, 30}
+	var assignedArray [3]int
+	fmt.Printf("aArray: %v\n", aArray)
+	assignedArray = aArray
+	assignedArray[1] = 888
+	fmt.Printf("aArray: %v\n", aArray)
+	fmt.Printf("assignedArray: %v\n", assignedArray)
+}
+
 func handle_args() {
+	piTest := flag.Int("int", 123, "Test a int flag")
+	fmt.Printf("piTest: %v\n", piTest)
 	flag.StringVar(&gFind, "find", "", "Specify the word to find")
 	flag.StringVar(&gBasePath, "basepath", gBasePath, "Specify the dir containing files to search")
 	flag.Parse()
@@ -47,5 +73,6 @@ func test_walkdir(sPath string) {
 func main() {
 	fmt.Println(PRG_NAME, PRG_VERSION)
 	handle_args()
+	test_go()
 	test_walkdir(gBasePath)
 }
