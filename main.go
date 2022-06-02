bb5fc037c18ad73b506174879cfef45335466c11
hanishkvc <hanishkvc@gmail.com>
2022-06-02 10:13:32 +0530

    Use flag library to parse cmdline args
    Get the string to search for

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 const PRG_NAME = "GoApropos"
 const PRG_VERSION = "v0-20220602IST0954"
 
+var gFind string
+
+func handle_args() {
+	flag.StringVar(&gFind, "find", "", "Specify the word to find")
+	flag.Parse()
+	fmt.Printf("gFind: %v\n", gFind)
+}
+
 func main() {
 	fmt.Println(PRG_NAME, PRG_VERSION)
+	handle_args()
 }
