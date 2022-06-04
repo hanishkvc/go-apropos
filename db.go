f0a522b221c250c9c501c61b8f2cea06eac49ce7
hanishkvc <hanishkvc@gmail.com>
2022-06-04 12:12:08 +0530

    DB: Cleanup db_find title print, add basepath also

--- db.go
+++ db.go
@@ -25,7 +25,7 @@ func db_print() {
 }
 
 func db_find(sFind string) {
-	fmt.Printf("\n%v:INFO: Possible matches for: %v\n", PRG_TAG, gFind)
+	fmt.Printf("\n%v:INFO: Possible matches for [%v] at [%v]\n", PRG_TAG, gFind, gBasePath)
 	pkgs := map[string][]string{}
 	sFU := strings.ToUpper(sFind)
 	for k, v := range gDB {
