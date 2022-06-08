eb65b74e2854c21af0c194b889abefe95a4ad9c4
hanishkvc <hanishkvc@gmail.com>
2022-06-08 19:14:30 +0530

    Helpers: Type parameters based generic map_print
    Added just for future, not using currently.

--- helpers.go
+++ helpers.go
@@ -15,6 +15,8 @@ func string_sort(theSlice []string) []string {
 	return theSlice
 }
 
+// THis is a sort of centralised map print logic
+// using pure Interface type mechanism to create a sort of generic function.
 func map_print(theMap any, sSep, sEnd string) {
 	switch m := theMap.(type) {
 	case map[string][]string:
@@ -26,5 +28,18 @@ func map_print(theMap any, sSep, sEnd string) {
 		for _, k := range keys {
 			fmt.Printf("%v%v%v%v", k, sSep, m[k], sEnd)
 		}
+	}
+}
+
+// THis is a sort of generic map print logic
+// using Type parameters support in the latest Go language versions (>= 1.18)
+func map_print_go118[GT any](theMap map[string]GT, sSep, sEnd string) {
+	keys := sort.StringSlice{}
+	for k, _ := range theMap {
+		keys = append(keys, k)
+	}
+	keys.Sort()
+	for _, k := range keys {
+		fmt.Printf("%v%v%v%v", k, sSep, theMap[k], sEnd)
 	}
 }
