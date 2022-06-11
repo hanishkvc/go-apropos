a6cac781792ff59f74bebf2be8000c0035a6a511
hanishkvc <hanishkvc@gmail.com>
2022-06-11 17:10:20 +0530

    Move building of the symbols+ db to its own go file

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/fs"
 	"os"
 	"strings"
 )
@@ -119,57 +118,6 @@ func handle_args() {
 		fmt.Printf("%v:INFO:ARG: gbCaseSensitive: %v\n", PRG_TAG, gbCaseSensitive)
 		fmt.Printf("%v:INFO:ARG: gsMatchMode: %v\n", PRG_TAG, gsMatchMode)
 	}
-}
-
-func handle_file(sFile string) {
-	if !strings.HasSuffix(sFile, "go") {
-		return
-	}
-	if strings.HasSuffix(sFile, "_test.go") {
-		return
-	}
-	for _, mpath := range gSkipFiles {
-		if strings.Contains(sFile, mpath) {
-			return
-		}
-	}
-	name, cmts, idents := gosrc_info(sFile)
-	if gFindPkg != FINDPKG_DEFAULT {
-		if !match_ok(name, gFindPkgP) {
-			return
-		}
-	}
-	db_add(name, sFile, cmts, idents)
-}
-
-func do_walkdir(sPath string) {
-	oFS := os.DirFS(sPath)
-	if giDEBUG > 10 {
-		fmt.Printf("%v:INFO:WALKDIR: oFS: %v\n", PRG_TAG, oFS)
-	}
-	fs.WalkDir(oFS, ".", func(path string, de fs.DirEntry, err error) error {
-		if err != nil {
-			fmt.Printf("%v:ERRR:WALKDIR: path: %v, Err: %v\n", PRG_TAG, path, err)
-			return err
-		}
-		var sPType string
-		deT := de.Type()
-		if deT.IsDir() {
-			sPType = "Dir"
-		} else if deT.IsRegular() {
-			sPType = "File"
-		} else {
-			sPType = "???"
-		}
-		if giDEBUG > 2 {
-			fmt.Printf("%v:INFO:WALKDIR: %v:path: %v\n", PRG_TAG, sPType, path)
-		}
-		if sPType == "File" {
-			theFile := sPath + string(os.PathSeparator) + path
-			handle_file(theFile)
-		}
-		return nil
-	})
 }
 
 func main() {
