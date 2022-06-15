18c4b4a7153dc09a124c8530ee66a659b3434342
hanishkvc <hanishkvc@gmail.com>
2022-06-15 18:30:32 +0530

    Main+: Update version and Note about cache usage at top of REadme

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 const PRG_TAG = "GOAPRO"
 const PRG_NAME = "GoApropos"
-const PRG_VERSION = "v5-20220614IST2022"
+const PRG_VERSION = "v5-20220615IST1830"
 
 const FIND_DUMMY = "__FIND_DUMMY__"
 const FINDPKG_DEFAULT = ""
