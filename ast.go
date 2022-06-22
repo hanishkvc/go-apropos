00000ab1447945753485211ebc33c2382b347cf0
hanishkvc <hanishkvc@gmail.com>
2022-06-23 00:14:31 +0530

    Ast: re clips / prefix in basepath, little bit more verbose

--- ast.go
+++ ast.go
@@ -158,16 +158,13 @@ func gosrc_info(sFile string) (string, string, DBSymbols) {
 		fmt.Printf("%v:DBUG:AST: GoFile:%v:%v:%v\n", PRG_TAG, sFile, gIdentyStats, gIdentyStats.delta_summary())
 	}
 	if gbPrependPkgBasePath {
-		re := regexp.MustCompile(fmt.Sprintf("%v(.*?)/%v.*", gBasePath, pkgName))
+		re := regexp.MustCompile(fmt.Sprintf("%v/(.*?)/%v.*", gBasePath, pkgName))
 		sMatchs := re.FindStringSubmatch(sFile)
-		if giDEBUG > 5 {
-			fmt.Printf("%v:%v:%v:%v:%v\n", PRG_TAG, re.String(), sFile, pkgName, sMatchs)
+		if giDEBUG > 1 {
+			fmt.Printf("%v:DBUG:AST:%v:%v:%v:%v\n", PRG_TAG, re.String(), sFile, pkgName, sMatchs)
 		}
 		if len(sMatchs) > 1 {
 			sPkgPrefix := sMatchs[1]
-			if (len(sPkgPrefix) > 0) && (sPkgPrefix[0] == '/') {
-				sPkgPrefix = sPkgPrefix[1:]
-			}
 			if len(sPkgPrefix) > 0 {
 				pkgName = sPkgPrefix + "/" + pkgName
 			}
